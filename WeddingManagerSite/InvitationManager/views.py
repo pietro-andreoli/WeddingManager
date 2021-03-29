9c31199bb1ef5f62039cbb970053fc7ce86bf6cd
Pietro Andreoli <peterandreoli96@gmail.com>
2021-03-28 22:36:04 -0400

    Added base views

--- WeddingManagerSite/InvitationManager/views.py
+++ WeddingManagerSite/InvitationManager/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+def index(request):
+	return HttpResponse("Hello world! Welcome to the InvitationManager app!")
+
+def invitation_endpoint(request, invitation_id):
+	return HttpResponse(f"Invitation Endpoint: {invitation_id}")
