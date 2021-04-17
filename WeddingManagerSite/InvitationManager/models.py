9efc1b4348a3ee35f68c70901e766e185c26b2f7
Pietro Andreoli <peterandreoli96@gmail.com>
2021-04-17 17:53:54 -0400

    Added options for fields in Guest model, changed invitation __str__

--- WeddingManagerSite/InvitationManager/models.py
+++ WeddingManagerSite/InvitationManager/models.py
@@ -24,20 +24,21 @@ class Invitation_Email(models.Model):
 class Invitation(models.Model):
 	# Uniquely identifies each row
 	invitation_id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+	invitation_name = models.CharField(unique=True, max_length=128, default="NO NAME SET")
 	# The ID used in the URL in the invitation email
-	invitation_url_id = models.CharField(max_length=32, null=False, unique=True)
+	invitation_url_id = models.CharField(max_length=36, null=False, unique=True, blank=True, default=uuid.uuid4)
 	# States whether the invitation has been sent already
 	invitation_sent = models.BooleanField(default=False)
 	# Date + time sent
-	send_date = models.DateTimeField(null=True)
+	send_date = models.DateTimeField(null=True, blank=True)
 	# States whether the invitation link has been clicked already
 	invitation_seen = models.BooleanField(default=False)
 	# The most recent date the link has been clicked
-	seen_date = models.DateTimeField(null=True)
-	assoc_email = models.ForeignKey(Invitation_Email, null=True, on_delete=models.SET_NULL)
+	seen_date = models.DateTimeField(null=True, blank=True)
+	assoc_email = models.ForeignKey(Invitation_Email, null=True, on_delete=models.SET_NULL, blank=True)
 
 	def __str__(self):
-		return self.invitation_url_id
+		return self.invitation_name
 
 class Group(models.Model):
 	group_label = models.CharField(primary_key=True, max_length=64, null=False, unique=True)
@@ -47,14 +48,18 @@ class Group(models.Model):
 		return self.group_label
 
 class Guest(models.Model):
+	WHOSE_GUEST_OPTIONS = [
+		("peter", "Peter's"),
+		("tea", "Teodora's")
+	]
 	guest_id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
 	first_name = models.CharField(max_length=16)
 	last_name = models.CharField(max_length=32)
 	relation = models.ForeignKey(Guest_Relation, null=True, on_delete=models.SET_NULL)
-	whose_guest = models.CharField(max_length=8, null=True)
-	home_address = models.CharField(max_length=128, null=True)
-	phone_number = models.CharField(max_length=16, null=True)
-	email = models.CharField(max_length=64, null=True)
+	whose_guest = models.CharField(max_length=8, null=True, choices=WHOSE_GUEST_OPTIONS)
+	home_address = models.CharField(max_length=128, null=True, blank=True)
+	phone_number = models.CharField(max_length=16, null=True, blank=True)
+	email = models.CharField(max_length=64, null=True, blank=True)
 	fb_link = models.CharField(max_length=256, null=True, blank=True)
 	assoc_group = models.ForeignKey(Group, null=True, on_delete=models.SET_NULL)
 	is_overseas = models.BooleanField(default=False)
