6e4dc6814cd32bbe3471c167bd4cd7fcb99fbb9a
Pietro Andreoli <peterandreoli96@gmail.com>
2022-08-31 21:33:25 -0400

    adding static

--- WeddingManagerSite/WeddingManagerSite/settings.py
+++ WeddingManagerSite/WeddingManagerSite/settings.py
@@ -101,7 +101,7 @@ INSTALLED_APPS = [
 	'django.contrib.contenttypes',
 	'django.contrib.sessions',
 	'django.contrib.messages',
-	# 'django.contrib.staticfiles',
+	'django.contrib.staticfiles',
 	"InvitationManager.apps.InvitationmanagerConfig",
 ]
 
@@ -192,4 +192,5 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
-STATIC_URL = '/static/'
+# STATIC_ROOT = BASE_DIR / "staticfiles"
+STATIC_URL = "/static/"
