a3102aefa7e0b3ff96bc94972aea602f3d9b7e30
Pietro Andreoli <peterandreoli96@gmail.com>
2022-09-17 16:12:57 -0400

    handle missing invitation bug

--- WeddingManagerSite/InvitationManager/urls.py
+++ WeddingManagerSite/InvitationManager/urls.py
@@ -9,6 +9,7 @@ urlpatterns = [
 	path('', views.index, name='index'),
 	# path('yourinvitation/<str:invitation_id>', views.invitation_endpoint, name='invitation_endpoint'),
 	path('yourinvitation/<str:invitation_id>', views.InvitationHomepage.as_view(), name='invitation_endpoint'),
+	path('yourinvitation/', views.missing_invitation, name='invalid_invitation_endpoint'),
 	path("import/", views.ImportGuestsView.as_view(), name="db_import_endpoint"),
 	path("help/", views.help_page, name="help_endpoint"),
 	path("yourinvitation/rsvp/<str:invitation_id>", views.RSVPFormView.as_view(), name="rsvp"),
