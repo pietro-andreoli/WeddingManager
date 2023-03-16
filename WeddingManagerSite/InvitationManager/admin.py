ca5fee7c1049cc065aa07bd1ebf28631b587cd4b
Pietro Andreoli <peterandreoli96@gmail.com>
2023-03-15 22:18:50 -0400

    update config admin page

--- WeddingManagerSite/InvitationManager/admin.py
+++ WeddingManagerSite/InvitationManager/admin.py
@@ -8,7 +8,6 @@ from django.utils.html import format_html
 admin.site.register(Guest)
 admin.site.register(Group)
 admin.site.register(Guest_Relation)
-admin.site.register(Config)
 # @admin.site.register(RSVP)
 
 class InvitationAdmin(admin.ModelAdmin):
@@ -45,6 +44,12 @@ class LogEventAdmin(admin.ModelAdmin):
 	related_inv.short_description = "Invitation Group"
 	related_inv.admin_order_field = "invitation__invitation_name"
 
+class ConfigAdmin(admin.ModelAdmin):
+	model = Config
+
+	list_display = ("config_id", "is_main_config")
+
 admin.site.register(RSVP, RSVPAdmin)
 admin.site.register(LogEvent, LogEventAdmin)
 admin.site.register(Invitation, InvitationAdmin)
+admin.site.register(Config, ConfigAdmin)
